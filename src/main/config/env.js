e07cd834b552758378f3404034b725ac054fbdb4
Abdul Malik Khoiri <abdulchoiri19@gmail.com>
2021-03-01 11:00:23 +0700

    Change api token

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -2,5 +2,5 @@ module.exports = {
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/movies-api',
   port: 8000,
   omdbApiURL: process.env.OMDB_API_URL || 'http://www.omdbapi.com',
-  omdbApiKey: process.env.OMDB_API_TOKEN || 'faf7e5bb&s'
+  omdbApiKey: process.env.OMDB_API_TOKEN || 'faf7e5bb'
 }
