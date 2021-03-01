d55cbf5a71535b9b980b8cc81962cb9d8f0b24d5
Abdul Malik Khoiri <abdulchoiri19@gmail.com>
2021-03-01 19:24:51 +0700

    Change root url

--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const fg = require('fast-glob')
 
 module.exports = app => {
-  app.use('/api/v1', router)
+  app.use('/', router)
   fg.sync('**/src/main/routes/**routes.js').forEach(file => require(`../../../${file}`)(router))
 }
